fd374c13675961559be7436862f723a32e1fbf07
Ali <alasdair.daw@gmail.com>
2015-04-13 17:49:26 +0100

    Add post and get for todos

--- server/server.js
+++ server/server.js
@@ -1,20 +1,49 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var Todo = require('./models/todo');
+var mongoose   = require('mongoose');
+
+var app = express();
 var port = process.env.PORT || 8080;
 var router = express.Router();
+
+mongoose.connect('mongodb://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + '@ds039281.mongolab.com:39281/scratch');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/api/v1/', router);
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function (request, response) {
   response.json({ message: 'hello there'});
 });
 
-app.use('/api/v1/', router);
+// create a todo (accessed at POST http://localhost:8080/api/v1/todos)
+router.route('/todos')
+  .post(function (request, response) {
+    // create a new instance of the Todo model        
+    var todo = new Todo();
+    todo.title = request.body.title;
+    todo.isCompleted = request.body.isCompleted;
+
+    todo.save(function (error) {
+      if (error) {
+        response.send(error);
+      }
+
+      response.json({ message: 'Todo created!' });
+    });
+  })
+  .get(function (request, response) {
+    Todo.find(function (error, todos) {
+      if (error) {
+        response.send(error);
+      }
+      response.json(todos);
+    });
+  });
 
 app.listen(port);
 console.log('Server running on port ' + port);
